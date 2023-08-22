7d454c7320b4ff34268fd8a054068d01a51b4f80
Alexander Ryabokon <mr.alexander.ryabokon@gmail.com>
2023-08-22 19:32:51 +0300

    octopus-external-systems-client-14 Fix GitlabTestClient FT

--- gitlab-test-client/build.gradle.kts
+++ gitlab-test-client/build.gradle.kts
@@ -13,6 +13,11 @@ configure<ComposeExtension> {
     useComposeFiles.add("${projectDir}${File.separator}docker${File.separator}docker-compose.yml")
     waitForTcpPorts.set(true)
     captureContainersOutputToFiles.set(buildDir.resolve("docker_logs"))
+    environment.putAll(
+        mapOf(
+            "DOCKER_REGISTRY" to project.properties["docker.registry"]
+        )
+    )
 }
 
 dockerCompose.isRequiredBy(tasks["test"])
