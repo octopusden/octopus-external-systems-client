a33ec9d82325499797a59e6739145f681a2203d8
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2023-10-19 19:14:29 +0300

    Add repository export/import methods.

--- test-client-test-commons/src/main/kotlin/org/octopusden/octopus/infrastructure/common/test/BaseTestClientTest.kt
+++ test-client-test-commons/src/main/kotlin/org/octopusden/octopus/infrastructure/common/test/BaseTestClientTest.kt
@@ -1,5 +1,6 @@
 package org.octopusden.octopus.infrastructure.common.test
 
+import java.io.File
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -11,12 +12,13 @@ import org.octopusden.octopus.infrastructure.common.test.dto.NewChangeSet
 private const val TAG = "test_tag"
 private const val PROJECT = "test_project"
 private const val REPOSITORY = "test-repository"
+private const val FEATURE_BRANCH = "feature"
+private const val DEVELOP_BRANCH = "develop"
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 abstract class BaseTestClientTest(
     private val testClient: TestClient, vcsFormatter: String
 ) {
-
     abstract fun getTags(project: String, repository: String): Collection<TestTag>
     abstract fun getCommits(project: String, repository: String, branch: String): Collection<TestCommit>
     abstract fun createPullRequestWithDefaultReviewers(
@@ -39,67 +41,85 @@ abstract class BaseTestClientTest(
     fun testCommit() {
         testClient.commit(
             NewChangeSet(
-                "master commit 1",
+                "${BaseTestClient.DEFAULT_BRANCH} commit 1",
                 vcsUrl,
-                "master"
+                BaseTestClient.DEFAULT_BRANCH
             )
         )
         testClient.commit(
             NewChangeSet(
-                "master commit 2",
+                "${BaseTestClient.DEFAULT_BRANCH} commit 2",
                 vcsUrl,
-                "master"
+                BaseTestClient.DEFAULT_BRANCH
             )
         )
         val firstDevelopCommitId = testClient.commit(
             NewChangeSet(
-                "develop commit 1",
+                "$DEVELOP_BRANCH commit 1",
                 vcsUrl,
-                "develop"
+                DEVELOP_BRANCH
             )
         )
         testClient.commit(
             NewChangeSet(
-                "develop commit 2",
+                "$DEVELOP_BRANCH commit 2",
                 vcsUrl,
-                "develop"
+                DEVELOP_BRANCH
             )
         )
         testClient.commit(
             NewChangeSet(
-                "feature commit 1",
+                "$FEATURE_BRANCH commit 1",
                 vcsUrl,
-                "feature"
+                FEATURE_BRANCH
             ),
             firstDevelopCommitId.id
         )
 
-        checkCommits("master", listOf("master commit 2", "master commit 1", "initial commit"))
         checkCommits(
-            "develop",
-            listOf("develop commit 2", "develop commit 1", "master commit 2", "master commit 1", "initial commit")
+            BaseTestClient.DEFAULT_BRANCH,
+            listOf(
+                "${BaseTestClient.DEFAULT_BRANCH} commit 2",
+                "${BaseTestClient.DEFAULT_BRANCH} commit 1",
+                BaseTestClient.INITIAL_COMMIT_MESSAGE
+            )
         )
         checkCommits(
-            "feature",
-            listOf("feature commit 1", "develop commit 1", "master commit 2", "master commit 1", "initial commit")
+            DEVELOP_BRANCH,
+            listOf(
+                "$DEVELOP_BRANCH commit 2",
+                "$DEVELOP_BRANCH commit 1",
+                "${BaseTestClient.DEFAULT_BRANCH} commit 2",
+                "${BaseTestClient.DEFAULT_BRANCH} commit 1",
+                BaseTestClient.INITIAL_COMMIT_MESSAGE
+            )
+        )
+        checkCommits(
+            FEATURE_BRANCH,
+            listOf(
+                "$FEATURE_BRANCH commit 1",
+                "$DEVELOP_BRANCH commit 1",
+                "${BaseTestClient.DEFAULT_BRANCH} commit 2",
+                "${BaseTestClient.DEFAULT_BRANCH} commit 1",
+                BaseTestClient.INITIAL_COMMIT_MESSAGE
+            )
         )
     }
 
     @Test
     fun testCommitBranchExistsAndParentException() {
         assertThrows<IllegalArgumentException> {
-            testClient.commit(NewChangeSet("message", vcsUrl, "develop"), "leftId")
+            testClient.commit(NewChangeSet("message", vcsUrl, DEVELOP_BRANCH), "leftId")
         }
-
-        val commitId = testClient.commit(NewChangeSet("message", vcsUrl, "develop")).id
+        val commitId = testClient.commit(NewChangeSet("message", vcsUrl, DEVELOP_BRANCH)).id
         assertThrows<IllegalArgumentException> {
-            testClient.commit(NewChangeSet("message", vcsUrl, "develop"), commitId)
+            testClient.commit(NewChangeSet("message", vcsUrl, DEVELOP_BRANCH), commitId)
         }
     }
 
     @Test
     fun testTag() {
-        val expectedId = testClient.commit(NewChangeSet("test tag commit", vcsUrl, "master")).id
+        val expectedId = testClient.commit(NewChangeSet("tag commit", vcsUrl, BaseTestClient.DEFAULT_BRANCH)).id
         testClient.tag(vcsUrl, expectedId, TAG)
         Assertions.assertEquals(TAG, getTags(PROJECT, REPOSITORY).first().displayId)
         Assertions.assertEquals(expectedId, getTags(PROJECT, REPOSITORY).first().commitId)
@@ -110,7 +130,7 @@ abstract class BaseTestClientTest(
         assertThrows<IllegalArgumentException> {
             testClient.tag(vcsUrl, "", TAG)
         }
-        testClient.commit(NewChangeSet("message", vcsUrl, "develop")).id
+        testClient.commit(NewChangeSet("message", vcsUrl, DEVELOP_BRANCH)).id
         assertThrows<IllegalArgumentException> {
             testClient.tag(vcsUrl, "left", TAG)
         }
@@ -118,20 +138,62 @@ abstract class BaseTestClientTest(
 
     @Test
     fun testPullRequest() {
-        val mainBranch = "master"
-        testClient.commit(NewChangeSet("initial commit", vcsUrl, mainBranch))
-        val featureBranch = "feature"
-        testClient.commit(NewChangeSet("feature commit", vcsUrl, featureBranch))
-
+        testClient.commit(
+            NewChangeSet(
+                "${BaseTestClient.DEFAULT_BRANCH} commit",
+                vcsUrl,
+                BaseTestClient.DEFAULT_BRANCH
+            )
+        )
+        testClient.commit(NewChangeSet("$FEATURE_BRANCH commit", vcsUrl, FEATURE_BRANCH))
         val pullRequest = createPullRequestWithDefaultReviewers(
             PROJECT,
             REPOSITORY,
-            featureBranch,
-            mainBranch,
+            FEATURE_BRANCH,
+            BaseTestClient.DEFAULT_BRANCH,
             "PR Title",
             "PR Description"
         )
         Assertions.assertTrue(pullRequest.id > 0)
+    }
+
+    @Test
+    fun testExportImport() {
+        testClient.commit(NewChangeSet("${BaseTestClient.DEFAULT_BRANCH} commit", vcsUrl, BaseTestClient.DEFAULT_BRANCH))
+        testClient.commit(NewChangeSet("$FEATURE_BRANCH commit", vcsUrl, FEATURE_BRANCH))
+        val tagCommitId = testClient.commit(NewChangeSet("tag commit", vcsUrl, BaseTestClient.DEFAULT_BRANCH)).id
+        testClient.tag(vcsUrl, tagCommitId, TAG)
+        val zip = File.createTempFile("TestClientTest", "zip").also { it.deleteOnExit() }
+        testClient.exportRepository(vcsUrl, zip)
+        testClient.clearData()
+        Assertions.assertThrows(Exception::class.java) {
+            getCommits(PROJECT, REPOSITORY, BaseTestClient.DEFAULT_BRANCH)
+        }
+        Assertions.assertThrows(Exception::class.java) {
+            getCommits(PROJECT, REPOSITORY, FEATURE_BRANCH)
+        }
+        Assertions.assertThrows(Exception::class.java) {
+            getTags(PROJECT, REPOSITORY)
+        }
+        testClient.importRepository(vcsUrl, zip)
+        checkCommits(
+            BaseTestClient.DEFAULT_BRANCH,
+            listOf(
+                "tag commit",
+                "${BaseTestClient.DEFAULT_BRANCH} commit",
+                BaseTestClient.INITIAL_COMMIT_MESSAGE
+            )
+        )
+        checkCommits(
+            FEATURE_BRANCH,
+            listOf(
+                "$FEATURE_BRANCH commit",
+                "${BaseTestClient.DEFAULT_BRANCH} commit",
+                BaseTestClient.INITIAL_COMMIT_MESSAGE
+            )
+        )
+        Assertions.assertEquals(TAG, getTags(PROJECT, REPOSITORY).first().displayId)
+        Assertions.assertEquals(tagCommitId, getTags(PROJECT, REPOSITORY).first().commitId)
     }
 
     private fun checkCommits(branch: String, expected: List<String>) {
