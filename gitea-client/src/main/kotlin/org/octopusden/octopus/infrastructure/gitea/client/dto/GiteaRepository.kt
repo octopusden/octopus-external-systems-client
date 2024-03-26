c003be268594ffeb1a295508ce90b6e77bfd76db
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2024-03-26 12:00:21 +0300

    Add author in gitea pull request dto. Extend gitea organization/repository dto. (#44)

--- gitea-client/src/main/kotlin/org/octopusden/octopus/infrastructure/gitea/client/dto/GiteaRepository.kt
+++ gitea-client/src/main/kotlin/org/octopusden/octopus/infrastructure/gitea/client/dto/GiteaRepository.kt
@@ -1,3 +1,12 @@
 package org.octopusden.octopus.infrastructure.gitea.client.dto
 
-data class GiteaRepository(val name: String) : BaseGiteaEntity()
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class GiteaRepository(
+    val id: Long,
+    val name: String,
+    val fullName: String,
+    val avatarUrl: String
+) : BaseGiteaEntity()
