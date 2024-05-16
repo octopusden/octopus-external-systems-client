f78de8b0a7f9198ef50a9b203be0d57ed99ba69a
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2024-05-16 18:07:25 +0300

    Support COPY and MOVE file changes for bitbucket. (#52)

--- bitbucket-client/src/main/kotlin/org/octopusden/octopus/infrastructure/bitbucket/client/dto/BitbucketCommitChange.kt
+++ bitbucket-client/src/main/kotlin/org/octopusden/octopus/infrastructure/bitbucket/client/dto/BitbucketCommitChange.kt
@@ -4,14 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class BitbucketCommitChange(
     val contentId: String,
+    val type: BitbucketCommitChangeType,
     val path: BitbucketCommitChangePath,
-    val type: BitbucketCommitChangeType
+    val srcPath: BitbucketCommitChangePath?
 ) : BaseBitbucketEntity<String>(contentId) {
     data class BitbucketCommitChangePath(
         @JsonProperty("toString")
         val value: String
     )
     enum class BitbucketCommitChangeType {
-        ADD, MODIFY, DELETE
+        ADD, MODIFY, DELETE, COPY, MOVE
     }
 }
