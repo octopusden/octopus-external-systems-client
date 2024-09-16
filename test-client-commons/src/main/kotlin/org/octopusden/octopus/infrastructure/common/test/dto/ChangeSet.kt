50b81abd87fdd63381794af67ff2835cacf34770
Alexander Ryabokon <mr.alexander.ryabokon@gmail.com>
2024-09-16 15:36:58 +0300

    Add branch field to ChangeSet

--- test-client-commons/src/main/kotlin/org/octopusden/octopus/infrastructure/common/test/dto/ChangeSet.kt
+++ test-client-commons/src/main/kotlin/org/octopusden/octopus/infrastructure/common/test/dto/ChangeSet.kt
@@ -2,16 +2,11 @@ package org.octopusden.octopus.infrastructure.common.test.dto
 
 import java.util.Date
 
-data class NewChangeSet(
-    val message: String,
-    val repository: String,
-    val branch: String
-)
-
 data class ChangeSet(
     val id: String,
     val message: String,
     val repository: String,
+    val branch: String,
     val author: String,
     val authorDate: Date
 )
---
+++ test-client-commons/src/main/kotlin/org/octopusden/octopus/infrastructure/common/test/dto/NewChangeSet.kt
@@ -0,0 +1,3 @@
+package org.octopusden.octopus.infrastructure.common.test.dto
+
+data class NewChangeSet(val message: String, val repository: String, val branch: String)
