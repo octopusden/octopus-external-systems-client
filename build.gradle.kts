6259661380344b7a55f59994cc114a95de430d50
Mark Rode <rode.mark01@gmail.com>
2025-08-15 11:10:30 +0300

    migrate gitea

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,6 +104,31 @@ subprojects {
         }
     }
 
+    ext {
+        System.getenv().let {
+            set("signingRequired", it.containsKey("ORG_GRADLE_PROJECT_signingKey") && it.containsKey("ORG_GRADLE_PROJECT_signingPassword"))
+            set("testPlatform", it.getOrDefault("TEST_PLATFORM", properties["test.platform"]))
+            set("dockerRegistry", it.getOrDefault("DOCKER_REGISTRY", properties["docker.registry"]))
+            set("octopusGithubDockerRegistry", it.getOrDefault("OCTOPUS_GITHUB_DOCKER_REGISTRY", project.properties["octopus.github.docker.registry"]))
+            set("okdActiveDeadlineSeconds", it.getOrDefault("OKD_ACTIVE_DEADLINE_SECONDS", properties["okd.active-deadline-seconds"]))
+            set("okdProject", it.getOrDefault("OKD_PROJECT", properties["okd.project"]))
+            set("okdClusterDomain", it.getOrDefault("OKD_CLUSTER_DOMAIN", properties["okd.cluster-domain"]))
+            set("okdWebConsoleUrl", (it.getOrDefault("OKD_WEB_CONSOLE_URL", properties["okd.web-console-url"]) as String).trimEnd('/'))
+            set("bitbucketLicense", it.getOrDefault("BITBUCKET_LICENSE", properties["bitbucket.license"]))
+        }
+    }
+
+    val supportedTestPlatforms = listOf("docker", "okd")
+    if (project.ext["testPlatform"] !in supportedTestPlatforms) {
+        throw IllegalArgumentException("Test platform must be set to one of the following $supportedTestPlatforms. Start gradle build with -Ptest.platform=... or set env variable TEST_PLATFORM")
+    }
+    val mandatoryProperties = mutableListOf("dockerRegistry", "octopusGithubDockerRegistry")
+    if (project.ext["testPlatform"] == "okd") {
+        mandatoryProperties.add("okdActiveDeadlineSeconds")
+        mandatoryProperties.add("okdProject")
+        mandatoryProperties.add("okdClusterDomain")
+    }
+
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
     }
