0bce51328bb8a91dbbc4d575fbc9ee9dec4cdab9
Gabriella Naomi Hutagalung <gabriellanaomi37@gmail.com>
2025-11-20 09:58:34 +0700

    Remove space

--- teamcity-client/src/main/kotlin/org/octopusden/octopus/infrastructure/teamcity/client/dto/TeamcityInvestigation.kt
+++ teamcity-client/src/main/kotlin/org/octopusden/octopus/infrastructure/teamcity/client/dto/TeamcityInvestigation.kt
@@ -1,7 +1,7 @@
 package org.octopusden.octopus.infrastructure.teamcity.client.dto
 
 data class TeamcityInvestigation(
-    val state : String,
+    val state: String,
     val assignee: TeamcityAssignee? = null,
     val assignment: TeamcityAssignment? = null
 )
