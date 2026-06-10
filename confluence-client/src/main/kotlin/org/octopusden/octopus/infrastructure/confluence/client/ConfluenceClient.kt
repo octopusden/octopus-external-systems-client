86611b4d6d8255b52a08ffc4532c85ac4d17d753
Mark Rode <rode.mark01@gmail.com>
2026-06-10 18:27:08 +0300

    Refactor

--- confluence-client/src/main/kotlin/org/octopusden/octopus/infrastructure/confluence/client/ConfluenceClient.kt
+++ confluence-client/src/main/kotlin/org/octopusden/octopus/infrastructure/confluence/client/ConfluenceClient.kt
@@ -10,8 +10,6 @@ import org.octopusden.octopus.infrastructure.confluence.client.dto.ConfluencePag
 const val REST_API_PATH = "rest/api"
 const val CONTENT_PATH = "$REST_API_PATH/content"
 
-const val DEFAULT_EXPAND = "body.storage,version,space,ancestors"
-
 interface ConfluenceClient {
 
     @RequestLine("GET $CONTENT_PATH/{id}")
@@ -24,8 +22,3 @@ interface ConfluenceClient {
     @Headers("Content-Type: application/json")
     fun updatePage(@Param("id") id: String, page: ConfluencePageUpdateRequest): ConfluencePage
 }
-
-fun ConfluenceClient.getPageById(
-    id: String,
-    expand: String = DEFAULT_EXPAND
-): ConfluencePage = getPageById(id, mapOf("expand" to expand))
