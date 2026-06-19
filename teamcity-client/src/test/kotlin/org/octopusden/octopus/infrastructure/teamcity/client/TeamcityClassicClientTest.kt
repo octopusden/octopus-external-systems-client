221d15724367681c7b0c804d5075348da25411ba
Gabriella Naomi Hutagalung <gabriellanaomi37@gmail.com>
2026-06-19 10:41:41 +0700

    Fix test

--- teamcity-client/src/test/kotlin/org/octopusden/octopus/infrastructure/teamcity/client/TeamcityClassicClientTest.kt
+++ teamcity-client/src/test/kotlin/org/octopusden/octopus/infrastructure/teamcity/client/TeamcityClassicClientTest.kt
@@ -492,7 +492,52 @@ class TeamcityClassicClientTest {
         val client = createClient(config)
         val project = createProject(client, "TestGetBuild")
         try {
+            val sourceBuildType = createBuildType(client, "TestGetBuildSourceType", project.id)
             val buildType = createBuildType(client, "TestGetBuildType", project.id)
+
+            val url = "ssh://git@github.com:octopusden/octopus-external-systems-client.git"
+            val vcsRoot = client.createVcsRoot(
+                TeamcityCreateVcsRoot(
+                    name = "${project.name}_VCS_ROOT",
+                    vcsName = TeamcityVCSType.GIT.value,
+                    projectLocator = project.id,
+                    properties = TeamcityProperties(
+                        listOf(
+                            TeamcityProperty("url", url),
+                            TeamcityProperty("branch", "master"),
+                            TeamcityProperty("authMethod", "PRIVATE_KEY_DEFAULT"),
+                            TeamcityProperty("userForTags", "tcagent"),
+                            TeamcityProperty("username", "git"),
+                            TeamcityProperty("ignoreKnownHosts", "true")
+                        )
+                    )
+                )
+            )
+            client.createBuildTypeVcsRootEntry(
+                buildType.id,
+                TeamcityCreateVcsRootEntry(
+                    id = vcsRoot.id,
+                    vcsRoot = TeamcityLinkVcsRoot(vcsRoot.id)
+                )
+            )
+            client.createSnapshotDependency(
+                buildType.id,
+                TeamcitySnapshotDependency(
+                    id = sourceBuildType.name!!,
+                    type = "snapshot_dependency",
+                    properties = TeamcityProperties(
+                        listOf(
+                            TeamcityProperty("run-build-if-dependency-failed", "MAKE_FAILED_TO_START"),
+                            TeamcityProperty("run-build-if-dependency-failed-to-start", "MAKE_FAILED_TO_START"),
+                            TeamcityProperty("run-build-on-the-same-agent", "false"),
+                            TeamcityProperty("take-started-build-with-same-revisions", "true"),
+                            TeamcityProperty("take-successful-builds-only", "true"),
+                        )
+                    ),
+                    sourceBuildType = TeamcityLinkBuildType(sourceBuildType.id)
+                )
+            )
+
             val queued = client.queueBuild(
                 TeamcityCreateQueuedBuild(
                     buildType = BuildTypeLocator(id = buildType.id),
