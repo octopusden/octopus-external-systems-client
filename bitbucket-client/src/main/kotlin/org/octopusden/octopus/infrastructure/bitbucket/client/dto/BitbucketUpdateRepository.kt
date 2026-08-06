9a176d46c90d5a8e6858d35370bc057af2a69289
Gabriella Naomi Hutagalung <gabriellanaomi37@gmail.com>
2026-08-06 15:47:45 +0700

    Add archived attribute in Bitbucket repository (#148)

--- bitbucket-client/src/main/kotlin/org/octopusden/octopus/infrastructure/bitbucket/client/dto/BitbucketUpdateRepository.kt
+++ bitbucket-client/src/main/kotlin/org/octopusden/octopus/infrastructure/bitbucket/client/dto/BitbucketUpdateRepository.kt
@@ -2,11 +2,14 @@ package org.octopusden.octopus.infrastructure.bitbucket.client.dto
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class BitbucketUpdateRepository
     @JsonCreator
     constructor(
         val name: String,
         val project: BitbucketProject,
+        val archived: Boolean? = null,
     )
